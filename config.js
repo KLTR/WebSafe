8819275d0241d71d8ab9bb112fb916451818e299
roylevy1990 <hotmailsux62@gmail.com>
2018-05-03 20:52:06 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,24 +1,24 @@
 module.exports = {
     twit: {
-        consumer_key: 'd4qnDOdvtcEni5E0P5b9zmmtJ',
-        consumer_secret: '3qgYh7N7mNmOKZblvxiWPH7g9Lc6CHupsvBYYyuK2u0IxvSxVi',
-        access_token: '325582425-Sw64Autsnbp3nKV6t5YHMOmfVdWz5GJluOlFjbcR',
-        access_token_secret: 'j2Da3OJu5SZasOPcQ1FShxvUuTFy8xXcbRsOnF4HVyxU5',
+        consumer_key: '',
+        consumer_secret: '',
+        access_token: '',
+        access_token_secret: '',
         timeout_ms: 60 * 1000, // optional HTTP request timeout to apply to all requests.
     },
     reddit: {
-        userAgent: 'put your user-agent string here',
-        clientId: '7Tv4hScrCf8p3g',
-        clientSecret: 'cN6dsMEGuaSlf-OTWEfa3mfmV18',
-        username: 'roylevyz',
-        password: 'AaAa1234'
+        userAgent: '',
+        clientId: '',
+        clientSecret: '',
+        username: '',
+        password: ''
     },
     firebase: {
-        apiKey: "AIzaSyAK9iQqatyi5RIK1GZ2TGLL4rFa0ibTo5Q",
-        authDomain: "websafe-bef72.firebaseapp.com",
-        databaseURL: "https://websafe-bef72.firebaseio.com",
-        projectId: "websafe-bef72",
-        storageBucket: "websafe-bef72.appspot.com",
-        messagingSenderId: "954840199739"
+        apiKey: "",
+        authDomain: "",
+        databaseURL: "",
+        projectId: "2",
+        storageBucket: "",
+        messagingSenderId: ""
     }
 }
